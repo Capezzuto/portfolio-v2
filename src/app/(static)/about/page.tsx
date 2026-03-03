83f887e56aeb2d13ce39dd505dcf29790725c63c
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-03 11:02:46 -0800

    Rename page-level components to use <Route>Page naming convention

--- src/app/(static)/about/page.tsx
+++ src/app/(static)/about/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const About = () => {
+const AboutPage = () => {
 	return <>About View</>;
 };
 
-export default About;
+export default AboutPage;
--- src/app/(static)/projects/page.tsx
+++ src/app/(static)/projects/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Projects = () => {
+const ProjectsPage = () => {
 	return <>Projects View</>;
 };
 
-export default Projects;
+export default ProjectsPage;
