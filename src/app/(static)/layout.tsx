bb36195e9bbcd6c8fcf28271b499c4e3ed8f8b8b
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-10 21:10:48 -0700

    Adjust column width for tablet

--- src/app/(static)/layout.tsx
+++ src/app/(static)/layout.tsx
@@ -4,7 +4,7 @@ import Header from './_components/Header';
 const StaticLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className='dynamic-background'>
-			<div className='max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl min-h-screen mx-auto px-4 md:px-0 flex flex-col flex-start'>
+			<div className='max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl min-h-screen mx-auto px-4 lg:px-0 flex flex-col flex-start'>
 				<Header />
 				<main className='flex-1 flex'>{children}</main>
 			</div>
