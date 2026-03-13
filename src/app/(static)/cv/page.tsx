fe1350228cd6981fcc95e11873c9b6873ea5a9e5
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-13 10:35:46 -0700

    Update cv page component to use StructuredJob and SubHeadline components

--- src/app/(static)/cv/page.tsx
+++ src/app/(static)/cv/page.tsx
@@ -2,6 +2,9 @@
 import { useCallback, useState } from 'react';
 import AnimatedLine from '@/app/_components/AnimatedLine';
 import Headline from '../_components/Headline';
+import StructuredJob from './StructuredJob';
+import workHistory from '@/app/_data/work-history.json';
+import SubHeadline from './SubHeadline';
 
 const CVPage = () => {
 	const [currentStage, setCurrentStage] = useState(1);
@@ -12,34 +15,54 @@ const CVPage = () => {
 	return (
 		<section className='cv-page'>
 			<Headline text='C.V.' />
-			<AnimatedLine
-				color='#171717'
-				viewBox='0 0 20 400'
-				width={20}
-				height={400}
-				length={400}
-				weight={2}
-				orientation='vertical'
-				shouldAnimate={currentStage >= 1}
-				animateType='forward'
-				path='M 10 0 v 10 400'
-				duration='1000ms'
-				delay='200ms'
-				onAnimationEnd={animationEndHandler}
-			/>
-			<AnimatedLine
-				height={20}
-				width={20}
-				color='oklch(55.6% 0 0)'
-				length={200}
-				weight={2}
-				shouldAnimate={currentStage >= 2}
-				viewBox='0 0 20 20'
-				path='M 10 1 a 9 9 0 1 1 0 18 a 9 9 0 1 1 0 -18'
-				animateType='outside-in'
-				duration='400ms'
-				onAnimationEnd={animationEndHandler}
-			/>
+			<section>
+				<SubHeadline text='Skills' />
+			</section>
+			<section>
+				<SubHeadline text='Experience' />
+				{workHistory.experience.map((job, i, arr) => (
+					<div key={job.id} className='flex'>
+						<div className=''>
+							<AnimatedLine
+								height={20}
+								width={20}
+								color='#62748e'
+								length={200}
+								weight={2}
+								shouldAnimate={currentStage >= 2 * i + 1}
+								viewBox='0 0 20 20'
+								path='M 10 1 a 9 9 0 1 1 0 18 a 9 9 0 1 1 0 -18'
+								animateType='outside-in'
+								duration='400ms'
+								onAnimationEnd={animationEndHandler}
+							/>
+							{i + 1 < arr.length ? (
+								<AnimatedLine
+									color='#62748e'
+									viewBox='0 0 20 400'
+									width={20}
+									height={400}
+									length={400}
+									weight={2}
+									orientation='vertical'
+									shouldAnimate={currentStage >= 2 * i + 2}
+									animateType='forward'
+									path='M 10 0 v 10 400'
+									duration='1000ms'
+									delay='200ms'
+									onAnimationEnd={animationEndHandler}
+								/>
+							) : null}
+						</div>
+						<div className='flex-1'>
+							<StructuredJob jobData={job} />
+						</div>
+					</div>
+				))}
+			</section>
+			<section>
+				<SubHeadline text='Education' />
+			</section>
 		</section>
 	);
 };
