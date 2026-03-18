5145bc527c2defe609bc09180cbe04696f850280
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-17 17:19:36 -0700

    Adjust layout and headline spacing

--- src/app/(static)/_components/Headline.tsx
+++ src/app/(static)/_components/Headline.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const Headline = ({ text }: { text: string }) => {
-	return <h1 className='font-(family-name:--font-fira-code) text-3xl md:text-4xl lg:text-5xl mt-20 mb-12'>{text}</h1>;
+	return <h1 className='font-(family-name:--font-fira-code) text-3xl md:text-4xl lg:text-5xl mb-8 md:mb-12'>{text}</h1>;
 };
 
 export default Headline;
--- src/app/(static)/layout.tsx
+++ src/app/(static)/layout.tsx
@@ -4,9 +4,9 @@ import Header from './_components/Header';
 const StaticLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className='dynamic-background'>
-			<div className='max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl min-h-screen mx-auto px-4 lg:px-0 flex flex-col flex-start'>
+			<div className='max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl min-h-screen mx-auto px-6 md:px-4 lg:px-0 flex flex-col flex-start'>
 				<Header />
-				<main className='flex-1 flex'>{children}</main>
+				<main className='flex-1 flex pt-16 pb-16 md:pt-20 md:pb-20'>{children}</main>
 			</div>
 		</div>
 	);
