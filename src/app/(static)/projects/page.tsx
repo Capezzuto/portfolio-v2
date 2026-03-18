e9caa1bf39362fc8b8ecf94077814ff2b21d77ca
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-18 10:58:42 -0700

    Create placeholder text for projects page

--- src/app/(static)/projects/page.tsx
+++ src/app/(static)/projects/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Headline from '../_components/Headline';
 
 const ProjectsPage = () => {
-	return <section className='projects-page'>Projects View</section>;
+	return (
+		<section className='projects-page'>
+			<Headline text='Projects' />
+			<p>Watch this space...</p>
+		</section>
+	);
 };
 
 export default ProjectsPage;
