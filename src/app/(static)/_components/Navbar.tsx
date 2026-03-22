a66075dbde314b941d16d3a733fd6a183d67020a
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-22 12:49:21 -0700

    Change menu in mobile to horizontal orientation, reuse desktop structure

--- src/app/(static)/_components/Navbar.tsx
+++ src/app/(static)/_components/Navbar.tsx
@@ -28,8 +28,32 @@ const tabs = [
 	},
 ];
 
-const Navbar = () => {
+const MenuOptions = () => {
 	const currentPath = usePathname();
+
+	return (
+		<>
+			<div className='dot-container pb-0.5 pointer-events-none'>
+				<span className='dot inline-block rounded-full w-2 h-2 bg-neutral-900 ml-8 mr-8 pointer-events-none'></span>
+			</div>
+			<ul className='flex justify-between'>
+				{tabs.map((tab) => (
+					<li key={tab.href} className='font-(family-name:--font-roboto-condensed) pb-1'>
+						<Link
+							id={tab.id}
+							href={tab.href}
+							className={`pb-0 pt-8 px-4 md:px-4 ${currentPath === tab.href ? ' active' : ''}`}
+						>
+							{tab.text}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
+
+const Navbar = () => {
 	const [showMenu, setShowMenu] = useState(false);
 
 	const toggleMenu = (evt: MouseEvent) => {
@@ -59,45 +83,24 @@ const Navbar = () => {
 	return (
 		<nav className={`Navbar group h-full`}>
 			<div className='Navbar-desktop group hidden h-full md:flex -mr-4 flex-col justify-end'>
-				<div className='dot-container pb-0.5 pointer-events-none'>
-					<span className='dot inline-block rounded-full w-2 h-2 bg-neutral-900 ml-8 mr-8 pointer-events-none'></span>
-				</div>
-				<ul className='flex justify-between'>
-					{tabs.map((tab, i, tabs) => (
-						<li key={tab.href} className='font-(family-name:--font-roboto-condensed) pb-1'>
-							<Link
-								id={tab.id}
-								href={tab.href}
-								className={`pb-0 pt-8 px-4 ${currentPath === tab.href ? ' active' : ''}`}
-							>
-								{tab.text}
-							</Link>
-						</li>
-					))}
-				</ul>
+				<MenuOptions />
 			</div>
-			<div className='Navbar-mobile group block md:hidden relative -mr-4'>
-				<button className='cursor-pointer px-4 py-2' onClick={toggleMenu}>
+			<div className='Navbar-mobile group block md:hidden relative'>
+				<button
+					className='cursor-pointer px-4 py-2 rounded-lg border border-neutral-500 border-b-2'
+					onClick={toggleMenu}
+				>
 					<span aria-hidden='true' className='block rounded-full w-1 h-1 bg-neutral-900 mb-1'></span>
 					<span aria-hidden='true' className='block rounded-full w-1 h-1 bg-neutral-900 mb-1'></span>
 					<span aria-hidden='true' className='block rounded-full w-1 h-1 bg-neutral-900'></span>
 				</button>
-				{showMenu && (
-					<ul className='flex-col absolute text-right right-0 pl-4 backdrop-blur-md rounded-lg border border-neutral-500'>
-						{tabs.map((tab, i, tabs) => (
-							<li key={tab.href + 'mobile'} className='font-(family-name:--font-roboto-condensed)'>
-								<Link
-									id={tab.id + ' mobile'}
-									href={tab.href}
-									onClick={(evt) => evt.stopPropagation()}
-									className={`Navbar-mobileOption block relative pl-5 pr-4 py-1.5 ${currentPath === tab.href ? ' active' : ''} after:bg-neutral-900 after:w-1.5 after:h-1.5 after:absolute after:rounded-full`}
-								>
-									{tab.text}
-								</Link>
-							</li>
-						))}
-					</ul>
-				)}
+
+				<div
+					aria-hidden={showMenu ? false : true}
+					className={`${showMenu ? 'opacity-100 pointer-events-auto' : 'opacity-0 translate-x-1/6 pointer-events-none'} absolute mt-1 right-0 px-0 py-0 backdrop-blur-md rounded-lg border border-neutral-500 border-b-2 transition transition-property-(--mobile-transition) ease-out duration-(--mobile-duration) delay-(--mobile-delay)`}
+				>
+					<MenuOptions />
+				</div>
 			</div>
 		</nav>
 	);
