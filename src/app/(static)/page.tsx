7936428385676177da2ff49c1262d0411faeb302
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-03-24 10:39:18 -0700

    Revise home page animation

--- src/app/(static)/page.tsx
+++ src/app/(static)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 const HomePage = () => {
 	return (
 		<section className='home-page self-center relative'>
-			<h1 className='font-(family-name:--font-fira-code) text-3xl text-transparent md:text-4xl lg:text-5xl pb-4 bg-linear-270 from-0% via-33% to-67% from-transparent to-neutral-900 via-transparent bg-clip-text bg-size-[300%_100%] bg-left animate-[scroll-fade_4s_ease-in-out_forwards]'>
+			<h1 className='font-(family-name:--font-fira-code) text-3xl text-transparent md:text-4xl lg:text-5xl pb-6 bg-(image:--ripple-gradient) bg-clip-text bg-no-repeat bg-size-[400%_100%] animate-(--ripple-animation)'>
 				Hi, take a look around.
 			</h1>
 		</section>
